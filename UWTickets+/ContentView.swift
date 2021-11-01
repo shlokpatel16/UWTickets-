553e2ebbbd1b03e046033db0174bc6ce48bb8890
Jacob Sweis <jakesweis@gmail.com>
2021-11-01 16:19:31 -0500

    Initial Commit

---
+++ UWTickets+/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UWTickets+
+//
+//  Created by Jacob Sweis on 11/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UWTickets+/UWTickets_App.swift
@@ -0,0 +1,17 @@
+//
+//  UWTickets_App.swift
+//  UWTickets+
+//
+//  Created by Jacob Sweis on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct UWTickets_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
